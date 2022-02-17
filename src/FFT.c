540b83d4213e25b6f378ff7563243eaba1e2eaef
Keanu van der Linden <keanu_jilderda@hotmail.com>
2022-02-17 17:00:07 +0100

    init

---
+++ src/FFT.c
@@ -0,0 +1,11 @@
+#include "FFT.h" 
+#include <stdio.h>
+#include "arm_math.h"
+//int applyFFT(int c);
+uint32_t fftSize = 1024;
+int applyFFT(int c){
+
+
+    return 6+c;
+}
+
---
+++ src/FFT.h
@@ -0,0 +1,7 @@
+
+#ifndef FFT_H_   /* Include guard */
+#define FFT_H_
+
+int applyFFT(int c);
+
+#endif // FOO_H_
